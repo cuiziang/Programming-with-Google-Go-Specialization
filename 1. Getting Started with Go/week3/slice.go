f080ea24fff3927b5ea5c14cd1d8c54e367cca85
Ziang Cui <mrchoisss@gmail.com>
2019-06-08 22:36:11 -0400

    Finish week 3

---
+++ 1. Getting Started with Go/week3/slice.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	sli := make([]int, 3)
+	i := -1
+
+	for true {
+		var s string
+		fmt.Print("Enter a num, exit if \"X\":\n")
+		_, _ = fmt.Scanln(&s)
+		ne, _ := strconv.Atoi(s)
+		if s == "X" {
+			break
+		}
+
+		if i < 2 {
+			i++
+			sli[i] = ne
+		} else {
+			sli = append(sli, ne)
+		}
+	}
+
+	for i, v := range sli {
+		fmt.Printf("A[%d]: %d\n", i, v)
+	}
+}
